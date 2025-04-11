8105a37121a3a3d1ae742864fbffb9a59cfd6a3c
Nez <193067990+Nez-Tenko@users.noreply.github.com>
2025-04-11 16:57:20 -0500

    Completed all functions and passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  for(const val of arr){
+    total += val;
+  }	
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = arr.reduce((total, currentVal) => {
+    return total * currentVal;
+  }, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  if(exponent <= 1){
+    return base;
+  }
+  else {
+    return (base*power(base,exponent-1));
+  }
+  
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if(x===0){
+    return 1;
+  }
+
+  let result = 1;
+  while(x) {
+    result *= x--;
+  }
+  return result;
 };
 
 // Do not edit below this line
