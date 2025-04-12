d37976ff9f1d05be7a559fab7e8283308b097034
Nez <193067990+Nez-Tenko@users.noreply.github.com>
2025-04-12 15:17:57 -0500

    Completed the palindrome function and passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const punctuation = /'".?!\s/;
+  let noPuncStr = str.replace(/['".?!,\s]/g, '');
+  noPuncStr = noPuncStr.toLowerCase();
+  /*for(let i=0; i<str.length/2; i++) {
+    if(str[i] !== str[str.length-1-i]) {
+      return false;
+    }
+  }
+  return true;*/
+
+  console.log("My test: " + noPuncStr);
+  for(let i=0; i<noPuncStr.length/2; i++) {
+    if(noPuncStr[i] !== noPuncStr[noPuncStr.length-1-i]) {
+      return false;
+    }
+  }
+  return true;
 
 };
 
