6cf8f47f905056bd28f2dcfe8bc4437badfe1fd8
Nez <193067990+Nez-Tenko@users.noreply.github.com>
2025-04-12 18:13:42 -0500

    Completed the getTheTitles function and passed alll tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(ob) {
+  let output = ob.map((item)=> {
+    return item.title;
+  });
+  return output;
 };
 
 // Do not edit below this line
