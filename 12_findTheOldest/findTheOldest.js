f4e4d695edd7533a3f46f4374253a2ae852a5b6f
Nez <193067990+Nez-Tenko@users.noreply.github.com>
2025-04-12 19:49:30 -0500

    Completed findTheOldest function and passed all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+ let oldestAge = 0;
+ let oldest = people.reduce((oldPerson, person) => {
+    //console.log('in function!');
+    let deathYear;
+    if(person.yearOfDeath === undefined) {
+      //console.log('person\'s year of death is undefined');
+      deathYear = new Date().getFullYear();
+    }
+    else {
+      //console.log("person's year of death is defined");
+      deathYear = person.yearOfDeath;
+    }
 
+    let currentAge = deathYear - person.yearOfBirth;
+    if(currentAge > oldestAge) {
+      oldestAge = currentAge;
+      return person;
+    }
+    else {
+      return oldPerson;
+    }
+  }, {}); 
+
+  console.log(oldest);
+  return oldest;
 };
 
 // Do not edit below this line
